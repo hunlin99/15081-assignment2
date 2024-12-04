50b894716aa0ad8986ace02425bff8107442cd5a
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-12-04 19:59:28 +0000

    install and test next.js

---
+++ my-project/app/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="logo">
+        CUISINES
+        <a href="#"></a>
+      </h1>
+      <nav>
+        <ul>
+          <li><a href="./">Home</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="./blog.html">Posts</a></li>
+          <li><a href="#" id="createPostBtn">Create Post</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ my-project/app/page.tsx
@@ -0,0 +1,110 @@
+import Image from "next/image";
+import './globals.css'; 
+
+export default function Home() {
+  return (
+<body>
+    <header>
+        <h1 className="logo">CUISINES<a href="#"></a></h1>
+        <nav>
+            <ul>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="./blog.html">Posts</a></li>
+                <li>
+                    <a href="#" id="createPostBtn">Create Post</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <div className="cover">
+            <h2>Cuisines around us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        </div>
+    </section>
+    <section className="trend">
+        <div className="latest">
+            <h1 className="text-3xl font-bold">
+                Trending article
+            </h1>
+            <div className="grid-container">
+                <div
+                    className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img className="rounded-t-lg w-full h-48 object-cover" src="./img/testimg-cafe.jpg" alt="" />
+                    </a>
+                    <div className="p-5">
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cafe Lander
+                            </h5>
+                        </a>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Industrial Loft Style cafe. Serve
+                            specialty coffees, breakfast, lunch & dessert.</p>
+                        <a href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img className="rounded-t-lg w-full h-48 object-cover" src="./img/testimg-pizza.jpg" alt="" />
+                    </a>
+                    <div className="p-5">
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lambretta
+                                Pizza Club</h5>
+                        </a>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A celebration of Italian culinary
+                            heritage with an international twist, featuring handcrafted pizzas, delectable pastas, and
+                            savory sandwiches,</p>
+                        <a href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div
+                    className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img className="rounded-t-lg w-full h-48 object-cover" src="./img/testimg-petit.jpg" alt="" />
+                    </a>
+                    <div className="p-5">
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Petit
+                                Potato</h5>
+                        </a>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A trendy Asian fusion restaurant
+                            and cafe offering a variety of food, tea-inspired snacks and dessert with Japanese flair.
+                        </p>
+                        <a href="#"
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="popular"></div>
+    </section>
+    <script src="./script.js"></script>
+</body>
+  );
+}
