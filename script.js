a8a75bf18fc882548c6457f0fd4a5ee23f698613
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-12-04 17:34:00 +0000

    creare DOM element variable

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const createPostBtn =
+        document.getElementById('createPostBtn');
+    const createPostModal =
+        document.getElementById('createPostModal');
+    const closeModal =
+        document.getElementById('closeModal');
+    const postForm =
+        document.getElementById('postForm');
+    const postSubmitBtn =
+        document.getElementById('postSubmitBtn');
+    const postContainer =
+        document.querySelector('.post-container');
+    const postDetailModal =
+        document.getElementById('postDetailModal');
+    const closeDetailModal =
+        document.getElementById('closeDetailModal');
+    const detailTitle =
+        document.getElementById('detailTitle');
+    const detailDate =
+        document.getElementById('detailDate');
+    const detailDescription =
+        document.getElementById('detailDescription');
+    const postImage =
+        document.getElementById('postImage'); 
+    const detailImage =
+        document.getElementById('detailImage'); 
+});
