e3620e54fd42a7178701ad274c4cd106f5bba33a
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-12-10 23:34:00 +0000

    Finish Responsive setting

--- my-project/app/contact-page/page.tsx
+++ my-project/app/contact-page/page.tsx
@@ -52,7 +52,7 @@ export default function ContactPage() {
           Contact Us
         </h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-          Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
+          Got a technical issue? Want to send feedback about a beta feature? Let us know.
         </p>
         <form onSubmit={handleSubmit} className="space-y-8">
           <div>
--- my-project/app/page.tsx
+++ my-project/app/page.tsx
@@ -59,8 +59,8 @@ export default function Home() {
             <Header />
             <section className="first-section">
                 <div className="cover">
-                    <h2>Cuisines around us</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <h2>Cuisines Around Us</h2>
+                    <p>Savor the Flavors, Discover the Stories</p>
                 </div>
             </section>
             <section className="trend">
@@ -92,48 +92,6 @@ export default function Home() {
                                 </div>
                             </div>
                         )}
-                        {/* Card 2 */}
-                        {/*<div
-                            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <img className="rounded-t-lg w-full h-48 object-cover" src="./img/testimg-pizza.jpg" alt="" />
-                            <div className="p-5">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lambretta
-                                    Pizza Club</h5>
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A celebration of Italian culinary
-                                    heritage with an international twist, featuring handcrafted pizzas, delectable pastas, and
-                                    savory sandwiches,</p>
-                                <button
-                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleOpenModal}>
-                                    Read more
-                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>*/}
-                        {/* Card 3 */}
-                        {/* <div
-                            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <img className="rounded-t-lg w-full h-48 object-cover" src="./img/testimg-petit.jpg" alt="" />
-                            <div className="p-5">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Petit
-                                    Potato</h5>
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">A trendy Asian fusion restaurant
-                                    and cafe offering a variety of food, tea-inspired snacks and dessert with Japanese flair.
-                                </p>
-                                <button
-                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleOpenModal}>
-                                    Read more
-                                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>*/}
                     </div>
                 </div>
             </section >
