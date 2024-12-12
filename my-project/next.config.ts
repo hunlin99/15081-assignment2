1ae767dbe8d942a9344623294c8f327cfad094c9
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2024-12-12 01:11:48 +0000

    change export path

--- my-project/next.config.ts
+++ my-project/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
-  basePath: '/my-project',
-  assetPrefix: '/my-project',
+  basePath: '/15081-assignment2',
+  assetPrefix: '/15081-assignment2',
 };
 
 export default nextConfig;
